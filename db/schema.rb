12fb689dc79680eb6a9c5aff19ca51892b3e9d00
davidaragao <davidaragao@gmail.com>
2012-07-02 22:01:55 -0300

    Location resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120702205350) do
+ActiveRecord::Schema.define(:version => 20120703010054) do
+
+  create_table "locations", :force => true do |t|
+    t.string   "latLong"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
